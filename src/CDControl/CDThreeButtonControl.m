7bbf4109458df16e760b839692d5d34fe43c37e4
Mark Carver <mark.carver@me.com>
2011-09-19 09:01:34 -0500

    Fixed label widths and heights, yet again. Found a few config foobars in the XIBs too.

--- src/CDControl/CDThreeButtonControl.m
+++ src/CDControl/CDThreeButtonControl.m
@@ -136,15 +136,15 @@
     float newHeight = [layoutManager usedRectForTextContainer:textContainer].size.height;
     float heightDiff = newHeight - labelRect.size.height;
     
+    // Set label's new width and height
+    NSRect l = NSMakeRect(labelRect.origin.x, labelRect.origin.y - heightDiff, labelRect.size.width, newHeight);
+    [expandingLabel setFrame: l];
+    
     // Set panel's new width and height
     NSSize p = [[panel contentView] frame].size;
 	p.height += heightDiff;
 	[panel setContentSize:p];
     [panel center];
-    
-    // Set label's new width and height
-    NSRect l = NSMakeRect(labelRect.origin.x, p.height - 20 - newHeight, labelRect.size.width, newHeight);
-    [expandingLabel setFrame: l];
 }
 
 
