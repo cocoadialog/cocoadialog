f41bf19a974132cb98910dfa47485899a428c9cf
Alex Gray <alex@mrgray.com>
2015-04-25 11:43:29 -0400

    reconfigured xcodeproj, removed unness. files

--- Source/AppController.m
+++ Source/AppController.m
@@ -18,8 +18,42 @@
 	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 */
 
+/*
+	AppController.h
+	cocoaDialog
+	Copyright (C) 2004 Mark A. Stratman <mark@sporkstorms.org>
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+ 
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+ 
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#import <Foundation/Foundation.h>
+#import <Dialogs/Dialogs.h>
 #import "CDUpdate.h"
-#import "AppController.h"
+
+@interface AppController : NSObject {
+    CDControl               *currentControl;
+    IBOutlet NSPanel        *aboutPanel;
+    IBOutlet NSTextField    *aboutAppLink;
+    IBOutlet NSTextField    *aboutText;
+    NSMutableArray          *arguments;
+}
+@end
+
+@interface NSAttributedString (Hyperlink)
++(id)hyperlinkFromString:(NSString*)inString withURL:(NSURL*)aURL withFont:(NSFont *)aFont;
+@end
 
 @implementation AppController
 
@@ -265,3 +299,10 @@
     return [attrString autorelease];
 }
 @end
+
+
+
+int main(int argc, const char *argv[])
+{
+	return NSApplicationMain(argc, argv);
+}
---
+++ dialogsTest/dialogsTest.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  dialogsTest
+//
+//  Created by Alex Gray on 4/25/15.
+//
+//
+
+@import Dialogs;
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+
+
+
+
+  }
+    return 0;
+}
