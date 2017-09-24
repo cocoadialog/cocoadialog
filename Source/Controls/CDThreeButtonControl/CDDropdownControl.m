d6a3fc850555601e9f0940385355c2aacb5ed528
Mark Carver <mark.carver@me.com>
2017-09-24 18:54:48 -0500

    fix(control): Temporarily account for dropdownControl position so it shows up in the right place

--- Source/Controls/CDThreeButtonControl/CDDropdownControl.m
+++ Source/Controls/CDThreeButtonControl/CDDropdownControl.m
@@ -86,5 +86,54 @@
 	}
 }
 
+// Should be called after setButtons, and before resize
+- (void) setLabel:(NSString *)labelText {
+    if (expandingLabel != nil) {
+        if (labelText == nil) {
+            labelText = @"";
+        }
+        float labelLineHeight = 14.0f;
+        float labelNewHeight = -labelLineHeight;
+        NSRect labelRect = expandingLabel.frame;
+        float labelHeightDiff = labelNewHeight - labelRect.size.height;
+        if (![labelText isBlank]) {
+            expandingLabel.stringValue = labelText;
+            NSTextStorage *textStorage = [[NSTextStorage alloc] initWithString: labelText];
+            NSTextContainer *textContainer = [[NSTextContainer alloc] initWithContainerSize:NSMakeSize(labelRect.size.width, FLT_MAX)];
+            NSLayoutManager *layoutManager = [[NSLayoutManager alloc]init];
+            [layoutManager addTextContainer: textContainer];
+            [textStorage addLayoutManager: layoutManager];
+            [layoutManager glyphRangeForTextContainer:textContainer];
+            labelNewHeight = [layoutManager usedRectForTextContainer:textContainer].size.height + labelLineHeight;
+            labelHeightDiff = labelNewHeight - labelRect.size.height;
+            // Set label's new height
+            NSRect l = NSMakeRect(labelRect.origin.x, labelRect.origin.y - labelHeightDiff, labelRect.size.width, labelNewHeight);
+            expandingLabel.frame = l;
+        }
+        else {
+            [expandingLabel setHidden:YES];
+        }
+        // Set panel's new width and height
+        NSSize p = self.panel.contentView.frame.size;
+        p.height += labelHeightDiff;
+        [self.panel setContentSize:p];
+
+        // Set dropdownControl's new Y.
+        // @todo this should be done automatically in TBC using the controlItems.
+        if (dropdownControl != nil) {
+            NSPoint d = dropdownControl.frame.origin;
+            d.y -= labelHeightDiff;
+            [dropdownControl setFrameOrigin:d];
+        }
+
+        // Set controlMatrix's new Y.
+        if (controlMatrix != nil) {
+            NSPoint m = controlMatrix.frame.origin;
+            m.y -= labelHeightDiff;
+            [controlMatrix setFrameOrigin:m];
+        }
+    }
+}
+
 
 @end
